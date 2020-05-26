c9c1d71e93e4cc59e35af1901629d22dc0a55d4a
Thomas Ngo <tngo0508@gmai.com>
2020-05-26 10:19:26 -0700

    testing

--- backend/pull_data_from_alphavantage.py
+++ backend/pull_data_from_alphavantage.py
@@ -3,10 +3,32 @@
 import pandas
 import pprint
 from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
 from alphavantage_key import key
+from matplotlib.pyplot import figure
+import matplotlib.pyplot as plt
 ts = TimeSeries(key=key)
 # Get json object with the intraday data and another with  the call's metadata
 data, meta_data = ts.get_intraday('GOOGL')
 
-pprint.pprint(data)
-pprint.pprint(meta_data)
+# pprint.pprint(data)
+# pprint.pprint(meta_data)
+
+key = key
+# Chose your output format, or default to JSON (python dict)
+ts = TimeSeries(key, output_format='pandas')
+ti = TechIndicators(key)
+
+# Get the data, returns a tuple
+# aapl_data is a pandas dataframe, aapl_meta_data is a dict
+aapl_data, aapl_meta_data = ts.get_daily(symbol='AAPL')
+# aapl_sma is a dict, aapl_meta_sma also a dict
+aapl_sma, aapl_meta_sma = ti.get_sma(symbol='AAPL')
+
+
+# Visualization
+figure(num=None, figsize=(15, 6), dpi=80, facecolor='w', edgecolor='k')
+aapl_data['4. close'].plot()
+plt.tight_layout()
+plt.grid()
+plt.show()
