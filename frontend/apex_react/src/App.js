819168ad489ae8794b5c7ba189fa0fac6418a8da
Thomas Ngo <tngo0508@gmai.com>
2020-05-26 15:26:46 -0700

    try Apex on React

---
+++ frontend/apex_react/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import PopChart from "./components/PopChart";
+
+function App() {
+  return (
+    <div className="container">
+      <PopChart />
+    </div>
+  );
+}
+
+export default App;
